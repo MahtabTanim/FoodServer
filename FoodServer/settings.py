3c86197dd23953446ca426646f9449df8ab27a12
MahtabTanim <tanimbsmrstu@gmail.com>
2025-02-07 00:32:04 +0600

    Created UserProfile Based on User Model, Created Custom Signals , Configured Media

--- FoodServer/settings.py
+++ FoodServer/settings.py
@@ -118,6 +118,8 @@ STATICFILES_DIRS = [
     "FoodServer/static",
 ]
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
