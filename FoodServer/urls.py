9e3782d0d7a28f8876168bd41842c1283242c7b5
MahtabTanim <tanimbsmrstu@gmail.com>
2025-02-07 01:49:24 +0600

    Configured base.html & files , registerUser form template

--- FoodServer/urls.py
+++ FoodServer/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls.static import static
 
 from .views import home
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
+    path("accounts/", include("account.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
