0182b7552af67f19495320a4fead915826d93fbe
MahtabTanim <tanimbsmrstu@gmail.com>
2025-02-19 08:05:26 +0600

    Dynamic Restaurant Overview  Setup

---
+++ account/context_processors.py
@@ -0,0 +1,9 @@
+from vendor.models import Vendor
+
+
+def get_vendor(request):
+    try:
+        vendor = Vendor.objects.get(user=request.user)
+        return {"vendor": vendor}
+    except:
+        return {"vendor": None}
