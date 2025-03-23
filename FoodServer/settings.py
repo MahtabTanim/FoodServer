d96c16a797d38bd29b735fde9209b7d3279c9ee6
MahtabTanim <tanimbsmrstu@gmail.com>
2025-03-23 17:45:28 +0600

    new changes to go live

--- FoodServer/settings.py
+++ FoodServer/settings.py
@@ -15,7 +15,13 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "66.228.56.9",
+    "foodserver.shop",
+    "www.foodserver.shop",
+]
 
 
 # Application definition
